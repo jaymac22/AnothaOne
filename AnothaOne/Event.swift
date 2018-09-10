b5f49ef16fd7f48a76ca79aee229bd778f77cbbb
Noah <noahgomez.r@gmail.com>
2018-09-10 14:45:33 -0500

    class update

---
+++ AnothaOne/Event.swift
@@ -0,0 +1,13 @@
+//
+//  Event.swift
+//  AnothaOne
+//
+//  Created by Noah Gomez on 9/10/18.
+//  Copyright © 2018 noahgomez. All rights reserved.
+//
+
+import Foundation
+
+class Event {
+    
+}
